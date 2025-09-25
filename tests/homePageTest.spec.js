ac6e04225a374b2596d6e294cdf6b6342fb403b2
Daniel Yonev <daniel.yonev@gmail.com>
2025-09-25 22:07:46 +0300

    basic test

---
+++ tests/homePageTest.spec.js
@@ -0,0 +1,7 @@
+const {test, expect} = require('@playwright/test');
+
+test('Home Page', async ({page})=>{
+    await page.goto('https://demoblaze.com/index.html');
+    await expect(page).toHaveTitle('STORE');
+    await page.close();
+})
