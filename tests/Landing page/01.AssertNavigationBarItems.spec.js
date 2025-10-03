7fff559a2fcbfa44c53bb109b861c5e838045178
Daniel Yonev <daniel.yonev@gmail.com>
2025-10-03 14:46:30 +0300

    added locators, login function, url, and a few test cases

--- tests/Landing page/01.AssertNavigationBarItems.spec.js
+++ tests/Landing page/01.AssertNavigationBarItems.spec.js
@@ -1,6 +1,17 @@
-import { test } from '@playwright/test'
+import { test, expect } from '@playwright/test'
 import { Login } from '../../helpers/login';
 
 test('Assert navigation bar', async({page})=>{
-    await Login(page); 
+    await Login(page);
+    const navigationBarItems = page.locator("//ul//li[@class='oxd-main-menu-item-wrapper']");
+    
+    const items = await page.$$("//ul//li[@class='oxd-main-menu-item-wrapper']")
+
+    //print the items in the navigation bar
+    for(const item of items){
+        const itemText = await item.textContent();
+        console.log(itemText);
+    }
+    //expect number of items to be 12
+    await expect(navigationBarItems).toHaveCount(12);
 })
